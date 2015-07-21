837fcd7e887fd1ed144bc85b0aaf29c841bd05f8
AunCly <aurelien.clugery@gmail.com>
2015-07-21 14:32:29 +0200

    Add HighLight event on Drag'n'drop

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
-		interact('.font')
-	  	.draggable({});
-	  	interact("p, h2")
-	  	.dropzone({
-	    	ondrop: function (event) {
-		      	console.log(event.relatedTarget.id);
-		      	$(event.target).css('font-family', event.relatedTarget.id);
-	    	}
-	  	});
-	});
+	interact('.font').draggable({});
+	interact('.font').on('dragstart', test);
+	interact('.font').on('dragstop', testt);
+	interact('p, h2').on('dragenter', test2);
+	interact('p, h2').on('dragleave', test3)
+  	interact("p, h2")
+  	.dropzone({
+    	ondrop: function (event) {
+	      	console.log(event.relatedTarget.id);
+	      	$(event.target).css('font-family', event.relatedTarget.id);
+	      	$(event.target).css('border', 'none');
+	      	$(event.relatedTarget).css('border', 'none');
+    	}
+  	});
+});
+
+function test (event) {
+		$(event.target).css('border', 'solid 1px #F1433F');
+}
+
+function testt (event) {
+		$(event.target).css('border', 'none');
+}
+
+function test2 (event){
+	$(event.target).css('border', 'solid 1px #F1433F');
+}
+
+function test3 (event){
+	$(event.target).css('border', 'none');
+}
